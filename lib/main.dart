64ca99b723147bdd80a6d428a3a941ebddeccf26
nwj002 <nwj.shrestha@gmail.com>
2024-05-06 12:06:49 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:class_test/app/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:class_test/screen/grid_view_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  final String name = "Nawaraj";
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+            builder: (context) => const GridViewScreen(
+                  nameData: 'Nawaraj',
+                )),
+      ),
+    );
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Welcome Nawaraj Shrestha",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
